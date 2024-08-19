2ff984e73e94e93829408062159fa0e2c1227db2
lfenzo <lfenzo@protonmail.com>
2024-08-19 15:26:53 -0300

    feat: ollama models are now auto-pulled in the ollama server

--- langchain-app/app/summarizers/ollama/ollama_summarizer.py
+++ langchain-app/app/summarizers/ollama/ollama_summarizer.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import ollama
 from langchain_ollama import ChatOllama
 from langchain_core.caches import BaseCache
 from langchain_core.messages import AIMessage
@@ -18,8 +19,17 @@ class OllamaSummarizer(BaseSummarizer):
     ):
         self.model = model
         self.base_url = base_url
+        self._check_model_is_pulled_in_server()
         runnable = self.prompt | ChatOllama(model=self.model, base_url=self.base_url, cache=cache)
         super().__init__(runnable=runnable, **kwargs)
+
+    def _check_model_is_pulled_in_server(self) -> None:
+        client = ollama.Client(host=self.base_url)
+        if (
+            not client.list()['models']
+            or not any(self.model == model['name'] for model in client.list()['models'])
+        ):
+            client.pull(model=self.model)
 
     def render_summary(self, content):
         text = ""
