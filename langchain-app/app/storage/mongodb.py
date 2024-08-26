08daec8a405fc60bc6f67fbf4304463c57368d74
lfenzo <lfenzo@protonmail.com>
2024-08-26 14:55:12 -0300

    fix: prevent MongoDB from storing file larger than the maximum file size

--- langchain-app/app/storage/mongodb.py
+++ langchain-app/app/storage/mongodb.py
@@ -5,6 +5,9 @@ from pymongo import MongoClient
 from app.storage.base_store_manager import BaseStoreManager
 
 
+MAX_DOCUMENT_SIZE_IN_BYTES = 16_793_598  # from pymongo error message (~16MB)
+
+
 class MongoDBStoreManager(BaseStoreManager):
 
     def __init__(self, user: str, password: str, database_name: str, collection_name: str):
@@ -17,12 +20,18 @@ class MongoDBStoreManager(BaseStoreManager):
     def get_connection_string(self, user: str, password: str) -> str:
         return f"mongodb://{user}:{password}@mongodb:27017/"
 
+    def document_can_be_stored(self, document: bytes) -> bool:
+        return len(document) <= MAX_DOCUMENT_SIZE_IN_BYTES
+
     async def store_summary(self, summary: str, metadata: dict, document: bytes) -> str:
         collection = self.db[self.collection_name]
+        # currently mongodb can only store document of up to 16MB in size
+        document = Binary(document) if self.document_can_be_stored(document) else None
+
         document_id = collection.insert_one({
             "metadata": metadata,
             "summary": summary,
-            "original_document_in_bytes": Binary(document),
+            "original_document_in_bytes": document,
         }).inserted_id
         return str(document_id)
 
