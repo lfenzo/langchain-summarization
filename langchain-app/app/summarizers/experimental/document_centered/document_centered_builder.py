1b925bbed8893b81be2eaaf4effc0ac2152e2588
lfenzo <lfenzo@protonmail.com>
2024-09-17 16:23:24 -0300

    cleaup: removed dead code after refactor

--- langchain-app/app/summarizers/experimental/document_centered/document_centered_builder.py
+++
@@ -1,9 +0,0 @@
-from app.summarizers.ollama.ollama_builder import OllamaSummarizationBuilder
-from app.summarizers.experimental.document_centered.document_centered_summarizer import (
-    DocumentCenteredSummarizer
-)
-
-
-class DocumentCenteredSummarizationBuilder(OllamaSummarizationBuilder):
-    def build(self):
-        return DocumentCenteredSummarizer(**self.get_init_params())
--- langchain-app/app/summarizers/experimental/reader_centered/reader_centered_builder.py
+++
@@ -1,9 +0,0 @@
-from app.summarizers.ollama.ollama_builder import OllamaSummarizationBuilder
-from app.summarizers.experimental.reader_centered.reader_centered_summarizer import (
-    ReaderCenteredSummarizer
-)
-
-
-class ReaderCenteredSummarizationBuilder(OllamaSummarizationBuilder):
-    def build(self):
-        return ReaderCenteredSummarizer(**self.get_init_params())
